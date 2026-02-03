fbe8154e962344ccaa168d2b249a77c8bb8086e9
ViperEkura <3081035982@qq.com>
2026-02-03 17:15:25 +0800

    feat(attention): 简化代码流程

--- src/attention/flash_v1.cu
+++ src/attention/flash_v1.cu
@@ -1,11 +1,8 @@
 #include "kernels/attention.h"
 
-static constexpr int Bq = 16;
-static constexpr int Bkv = 16;
+static constexpr int Bl = 16;
 static constexpr int Bd = 16;
-static constexpr int KV_PER_Q = Bkv / Bq;
-
-// 32 > Bd >= Bkv >= Bq
+// Bd >= Bl
 
 __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
 {
@@ -15,34 +12,34 @@ __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
 
     const int tx = threadIdx.x, ty = threadIdx.y;
     const int bx = blockIdx.x, by = blockIdx.y, bz = blockIdx.z;
-    const int Tkv = (L_kv + Bkv - 1) / Bkv;
+    const int Tkv = (L_kv + Bl - 1) / Bl;
     const int Td = (D + Bd -1) / Bd;
     const float scale = param.scale;
 
     const int batch  = bz;
-    const int load_gmem_q = by * blockDim.y + ty;
-    const int load_gmem_d = bx * blockDim.x + tx;
-    const int load_smem_q = ty;
-    const int load_smem_d = tx;
     const int tid = ty * blockDim.x + tx;
+    const int load_gmem_l = by * blockDim.y + ty;
+    const int load_gmem_d = bx * blockDim.x + tx;
+    const int load_smem_l = ty;
+    const int load_smem_d = tx;
 
-    __shared__ float smem_q[Bq][Bd];
-    __shared__ float smem_k[Bkv][Bd];
-    __shared__ float smem_v[Bkv][Bd];
-    __shared__ float smem_sp[Bq][Bkv];
+    __shared__ float smem_q[Bl][Bd];
+    __shared__ float smem_k[Bl][Bd];
+    __shared__ float smem_v[Bl][Bd];
+    __shared__ float smem_sp[Bl][Bl];
 
     float row_max = -INFINITY;
     float row_sum = 0;
-    float reg_o = 0;
+    double reg_o = 0;
 
     for (int kv_stride = 0; kv_stride < Tkv; kv_stride++)
     {
-        int kv_start = kv_stride * Bkv;
+        int kv_start = kv_stride * Bl;
         // S = 0;
-        if(tid < Bq * Bkv)
+        if(tid < Bl * Bl)
         {
-            int local_q = tid / Bkv;
-            int local_k = tid % Bkv;
+            int local_q = tid / Bl;
+            int local_k = tid % Bl;
 
             for(int d = 0; d < Bd; d++)
             {
@@ -54,35 +51,32 @@ __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
         for(int d_stride = 0; d_stride < Td; d_stride++)
         {
             int d_start = d_stride * Bd;
-            int load_gmem_q_l = load_gmem_q;
+            int load_gmem_q_l = load_gmem_l;
             int load_gmem_q_d = d_start + load_smem_d;
             int load_gmem_q_addr = batch * L_q * D + load_gmem_q_l * D + load_gmem_q_d;
             // load Q, K
             if (load_gmem_q_l < L_q && load_gmem_q_d < D)
-                smem_q[load_smem_q][load_smem_d] = param.q_ptr[load_gmem_q_addr];
+                smem_q[load_smem_l][load_smem_d] = param.q_ptr[load_gmem_q_addr];
             else 
-                smem_q[load_smem_q][load_smem_d] = 0;
+                smem_q[load_smem_l][load_smem_d] = 0;
 
-            for (int i = 0; i < KV_PER_Q; i++)
-            {
-                int load_smem_kv_l = load_smem_q * KV_PER_Q + i;
-                int load_gmem_kv_l = kv_start + load_smem_kv_l;
-                int load_gmem_kv_d = d_start + load_smem_d;
-                int load_gmem_kv_addr = batch * L_kv * D + load_gmem_kv_l * D + load_gmem_kv_d;
-                
-                if (load_gmem_kv_l < L_kv && load_gmem_kv_d < D)
-                    smem_k[load_smem_q * KV_PER_Q + i][load_smem_d] = param.k_ptr[load_gmem_kv_addr];
-                else
-                    smem_k[load_smem_q * KV_PER_Q + i][load_smem_d] = 0;
-            }
+            int load_smem_kv_l = load_smem_l;
+            int load_gmem_kv_l = kv_start + load_smem_kv_l;
+            int load_gmem_kv_d = d_start + load_smem_d;
+            int load_gmem_kv_addr = batch * L_kv * D + load_gmem_kv_l * D + load_gmem_kv_d;
             
+            if (load_gmem_kv_l < L_kv && load_gmem_kv_d < D)
+                smem_k[load_smem_l][load_smem_d] = param.k_ptr[load_gmem_kv_addr];
+            else
+                smem_k[load_smem_l][load_smem_d] = 0;
+
             __syncthreads();
 
             // S = Q @ K.T
-            if(tid < Bq * Bkv)
+            if(tid < Bl * Bl)
             {
-                int local_q = tid / Bkv;
-                int local_k = tid % Bkv;
+                int local_q = tid / Bl;
+                int local_k = tid % Bl;
                 for(int d = 0; d < Bd; d++)
                 {
                     smem_sp[local_q][local_k] += smem_q[local_q][d] * smem_k[local_k][d] * scale;
@@ -94,25 +88,25 @@ __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
         float block_max = -INFINITY;
         float block_sum = 0;
         // block_max = max(S, dim=-1)
-        for (int  i = 0; i < Bkv; i++)
+        for (int  i = 0; i < Bl; i++)
         {
-            block_max = max(block_max, smem_sp[load_smem_q][i]);
+            block_max = max(block_max, smem_sp[load_smem_l][i]);
         }
         __syncthreads();
 
         // P = exp(S - block_max)
-        if(tid < Bq * Bkv)
+        if(tid < Bl * Bl)
         {
-            int local_q = tid / Bkv;
-            int local_k = tid % Bkv;
+            int local_q = tid / Bl;
+            int local_k = tid % Bl;
             smem_sp[local_q][local_k] = exp(smem_sp[local_q][local_k] - block_max);
         }
         __syncthreads();
 
         // block_sum = sum(P, dim=-1)
-        for (int  i = 0; i < Bkv; i++)
+        for (int  i = 0; i < Bl; i++)
         {
-            block_sum += smem_sp[load_smem_q][i];
+            block_sum += smem_sp[load_smem_l][i];
         }
 
         float new_max = max(block_max, row_max);
@@ -121,23 +115,21 @@ __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
         float pv = 0;
         
         // load V
-        for (int i = 0; i < KV_PER_Q; i++)
-        {
-            int load_smem_kv_l = load_smem_q * KV_PER_Q + i;
-            int load_gmem_kv_l = kv_start + load_smem_kv_l;
-            int load_gmem_kv_d = load_gmem_d;
-            int load_gmem_kv_addr = batch * L_kv * D + load_gmem_kv_l * D + load_gmem_kv_d;
-            
-            if (load_gmem_kv_l < L_kv && load_gmem_kv_d < D)
-                smem_v[load_smem_kv_l][load_smem_d] = param.v_ptr[load_gmem_kv_addr];
-            else
-                smem_v[load_smem_kv_l][load_smem_d] = 0;
-        }
+        int load_smem_kv_l = load_smem_l;
+        int load_gmem_kv_l = kv_start + load_smem_kv_l;
+        int load_gmem_kv_d = load_gmem_d;
+        int load_gmem_kv_addr = batch * L_kv * D + load_gmem_kv_l * D + load_gmem_kv_d;
+        
+        if (load_gmem_kv_l < L_kv && load_gmem_kv_d < D)
+            smem_v[load_smem_kv_l][load_smem_d] = param.v_ptr[load_gmem_kv_addr];
+        else
+            smem_v[load_smem_kv_l][load_smem_d] = 0;
+
         __syncthreads();
         // PV = P @ V
-        for(int kv = 0; kv < Bkv; kv++)
+        for(int kv = 0; kv < Bl; kv++)
         {
-            pv += smem_sp[load_smem_q][kv] * smem_v[kv][load_smem_d];
+            pv += smem_sp[load_smem_l][kv] * smem_v[kv][load_smem_d];
         }
         __syncthreads();
         
@@ -147,23 +139,23 @@ __global__ void sdqa_attention_fwd_flash_v1(attention_param_t param)
         row_max = new_max;
     }
 
-    int load_gmem_q_l = load_gmem_q;
-    int load_gmem_q_d = load_gmem_d;
-    int load_gmem_q_addr = batch * L_q * D + load_gmem_q_l * D + load_gmem_q_d;
+    int load_gmem_o_l = load_gmem_l;
+    int load_gmem_o_d = load_gmem_d;
+    int load_gmem_o_addr = batch * L_q * D + load_gmem_o_l * D + load_gmem_o_d;
 
-    if (load_gmem_q_l < L_q && load_gmem_q_d < D)
+    if (load_gmem_o_l < L_q && load_gmem_o_d < D)
     {
-        param.o_ptr[load_gmem_q_addr] = reg_o / (row_sum + param.eps);
+        param.o_ptr[load_gmem_o_addr] = (float)(reg_o / (row_sum + param.eps));
     }
     
 }
 
 void launch_sdqa_attention_fwd_flash_v1(attention_param_t param)
 {
-    dim3 block(Bd, Bq);
+    dim3 block(Bd, Bl);
     dim3 grid(
         (param.dim + Bd - 1) / Bd,
-        (param.len_q + Bq - 1) / Bq, 
+        (param.len_q + Bl - 1) / Bl, 
         param.batch
     );
     sdqa_attention_fwd_flash_v1<<<grid, block>>>(param);
