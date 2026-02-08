5891748d9f12ac93af61a3648912b3b0c4d135e8
ViperEkura <3081035982@qq.com>
2026-02-08 16:30:10 +0800

    fix(matmul): 修复cublas矩阵乘计算问题

--- src/matmul/cublas.cu
+++ src/matmul/cublas.cu
@@ -1,37 +1,29 @@
 #include <cublas_v2.h>
-#include <stdexcept>
-
 #include "kernels/matmul.h"
+#include "common.h"
 
 void launch_matmul_cublas(matmul_param_t param) {
     cublasHandle_t handle;
-    cublasStatus_t status = cublasCreate(&handle);
-    if (status != CUBLAS_STATUS_SUCCESS) {
-        throw std::runtime_error("cuBLAS handle creation failed");
-    }
+    CUBLAS_CHECK(cublasCreate(&handle));
 
     const float alpha = 1.0f;
     const float beta = 0.0f;
 
-    status = cublasSgemm(handle,
-                         CUBLAS_OP_N,
-                         CUBLAS_OP_N,
-                         param.M,
-                         param.N,
-                         param.K,
-                         &alpha,
-                         param.rhs,
-                         param.N, 
-                         param.lhs,
-                         param.K,
-                         &beta,
-                         param.dst,
-                         param.N);
-
-    if (status != CUBLAS_STATUS_SUCCESS) {
-        cublasDestroy(handle);
-        throw std::runtime_error("cuBLAS SGEMM failed");
-    }
+    CUBLAS_CHECK(cublasSgemm(handle,
+        CUBLAS_OP_N,
+        CUBLAS_OP_N,
+        param.N,
+        param.M,
+        param.K,
+        &alpha,
+        param.rhs,
+        param.N, 
+        param.lhs,
+        param.K,
+        &beta,
+        param.dst,
+        param.N
+    ));
 
     cudaDeviceSynchronize();
     cublasDestroy(handle);
