73c877687e18e4115d51f7a82a263fcbcf08ae6d
ViperEkura <3081035982@qq.com>
2026-02-10 17:16:45 +0800

    feat(utils): 更新工具函数

--- include/utils/reduce.cuh
+++ include/utils/reduce.cuh
@@ -3,23 +3,23 @@
 
 // ------------------ Warp-level reductions ------------------
 template<typename T>
-__forceinline__ __device__ T warpReduceMax(T val) {
-    for (int offset = 16; offset > 0; offset /= 2)
-        val = max(val, __shfl_down_sync(0xffffffff, val, offset));
+__forceinline__ __device__ T warpReduceMax(T val, int width=32) {
+    for (int offset = width / 2; offset > 0; offset /= 2)
+        val = max(val, __shfl_xor_sync(0xffffffff, val, offset, width));
     return val;
 }
 
 template<typename T>
-__forceinline__ __device__ T warpReduceMin(T val) {
-    for (int offset = 16; offset > 0; offset /= 2)
-        val = min(val, __shfl_down_sync(0xffffffff, val, offset));
+__forceinline__ __device__ T warpReduceMin(T val, int width=32) {
+    for (int offset = width / 2; offset > 0; offset /= 2)
+        val = min(val, __shfl_xor_sync(0xffffffff, val, offset, width));
     return val;
 }
 
 template<typename T>
-__forceinline__ __device__ T warpReduceSum(T val) {
-    for (int offset = 16; offset > 0; offset /= 2)
-        val += __shfl_down_sync(0xffffffff, val, offset);
+__forceinline__ __device__ T warpReduceSum(T val, int width=32) {
+    for (int offset = width / 2; offset > 0; offset /= 2)
+        val += __shfl_xor_sync(0xffffffff, val, offset, width);
     return val;
 }
 
