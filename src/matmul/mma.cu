9f57342ad79a021375c0286c4996d4fbfac7dc0f
ViperEkura <3081035982@qq.com>
2026-02-15 17:34:41 +0800

    feat(matmul): 增加mma 版本 向量化加载

--- src/matmul/mma.cu
+++ src/matmul/mma.cu
@@ -10,6 +10,8 @@ static constexpr int WMMA_M = 16;
 static constexpr int WMMA_N = 16;
 static constexpr int WMMA_K = 16;
 
+#define FLOAT4_REF(x)(*reinterpret_cast<float4*>((x)))
+#define HALF2_PTR(x)(reinterpret_cast<half2*>((x)))
 
 __global__ void matmul_wmma(matmul_param_t param)
 {
@@ -17,13 +19,15 @@ __global__ void matmul_wmma(matmul_param_t param)
     int N = param.N;
     int K = param.K;
     
-    const int tile_row = blockIdx.y * WMMA_M;
-    const int tile_col = blockIdx.x * WMMA_N;
+    const int m_start = blockIdx.y * WMMA_M;
+    const int n_start = blockIdx.x * WMMA_N;
+    const int num_tiles_k = (K + WMMA_K - 1) / WMMA_K;
 
     __shared__ half A_shared[WMMA_M * WMMA_K];
     __shared__ half B_shared[WMMA_K * WMMA_N];
-
-    const int num_tiles_k = (K + WMMA_K - 1) / WMMA_K;
+    
+    float4 lhs_vec, rhs_vec;
+    
     wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
     wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
     wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> acc_frag;
@@ -32,20 +36,39 @@ __global__ void matmul_wmma(matmul_param_t param)
     for (int tile_k = 0; tile_k < num_tiles_k; tile_k++) {
         const int k_offset = tile_k * WMMA_K;
         
-        for (int i = threadIdx.x; i < WMMA_M * WMMA_K; i += blockDim.x) {
-            int row_a = i / WMMA_K;
-            int col_a = i % WMMA_K;
-            if (tile_row + row_a < M && k_offset + col_a < K) {
-                A_shared[i] = __float2half(param.lhs[(tile_row + row_a) * K + (k_offset + col_a)]);
-            } else {
-                A_shared[i] = __float2half(0.0f);
+        for (int i = threadIdx.x; i < WMMA_M * WMMA_K / 4; i += blockDim.x) {
+            int base_idx = i * 4;
+            int row_a = base_idx / WMMA_K;
+            int col_a = base_idx % WMMA_K;
+            
+            if (m_start + row_a < M && k_offset + col_a < K && col_a + 3 < WMMA_K) {
+                float* src_ptr = param.lhs + (m_start + row_a) * K + (k_offset + col_a);
+                lhs_vec = FLOAT4_REF(src_ptr);
+                
+                float2 f2_0 = make_float2(lhs_vec.x, lhs_vec.y);
+                float2 f2_1 = make_float2(lhs_vec.z, lhs_vec.w);
+                
+                half2 h2_0 = __float22half2_rn(f2_0);
+                half2 h2_1 = __float22half2_rn(f2_1);
+                
+                HALF2_PTR(A_shared)[base_idx/2] = h2_0;
+                HALF2_PTR(A_shared)[base_idx/2 + 1] = h2_1;
             }
-            int row_b = i / WMMA_N;
-            int col_b = i % WMMA_N;
-            if (k_offset + row_b < K && tile_col + col_b < N) {
-                B_shared[i] = __float2half(param.rhs[(k_offset + row_b) * N + (tile_col + col_b)]);
-            } else {
-                B_shared[i] = __float2half(0.0f);
+            
+            int row_b = base_idx / WMMA_N;
+            int col_b = base_idx % WMMA_N;
+            if (k_offset + row_b < K && n_start + col_b < N && col_b + 3 < WMMA_N) {
+                float* src_ptr = param.rhs + (k_offset + row_b) * N + (n_start + col_b);
+                rhs_vec = FLOAT4_REF(src_ptr);
+                
+                float2 f2_0 = make_float2(rhs_vec.x, rhs_vec.y);
+                float2 f2_1 = make_float2(rhs_vec.z, rhs_vec.w);
+                
+                half2 h2_0 = __float22half2_rn(f2_0);
+                half2 h2_1 = __float22half2_rn(f2_1);
+                
+                HALF2_PTR(B_shared)[base_idx/2] = h2_0;
+                HALF2_PTR(B_shared)[base_idx/2 + 1] = h2_1;
             }
         }
 
@@ -58,8 +81,8 @@ __global__ void matmul_wmma(matmul_param_t param)
         __syncthreads();
     }
     
-    if (tile_row < M && tile_col < N) {
-        float* C_ptr = param.dst + tile_row * N + tile_col;
+    if (m_start < M && n_start < N) {
+        float* C_ptr = param.dst + m_start * N + n_start;
         wmma::store_matrix_sync(C_ptr, acc_frag, N, wmma::mem_row_major);
     }
 }
